d589bc764c6b0fd6df0c89e8baa80e08dd6bbfa0
ham-0915 <ham-0915@yandex.com>
2025-09-12 11:31:41 +0800

    Update diy-part2.sh

--- N1/Lede/diy-part2.sh
+++ N1/Lede/diy-part2.sh
@@ -30,8 +30,8 @@ git clone https://github.com/sbwml/openwrt_helloworld package/helloworld
 #sed -i 's#flto#flto -D_LARGEFILE64_SOURCE#g' feeds/packages/utils/acpid/Makefile
 #sed -i 's/stripped/release/g' feeds/packages/multimedia/aliyundrive-webdav/Makefile
 #sed -i 's#SYNC#SYNC -D_LARGEFILE64_SOURCE#g' feeds/packages/utils/xfsprogs/Makefile
-sed -i 's/REENTRANT -D_GNU_SOURCE/LARGEFILE64_SOURCE/g' feeds/packages/lang/perl/perlmod.mk
-sed -i 's#GO_PKG_TARGET_VARS.*# #g' feeds/packages/utils/v2dat/Makefile
+#sed -i 's/REENTRANT -D_GNU_SOURCE/LARGEFILE64_SOURCE/g' feeds/packages/lang/perl/perlmod.mk
+#sed -i 's#GO_PKG_TARGET_VARS.*# #g' feeds/packages/utils/v2dat/Makefile
 
 sed -i 's/-march=armv8-a+crypto/-march=armv8-a/g' openwrt/.config
 
